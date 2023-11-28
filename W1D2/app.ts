bb244e98e84eee9b6b83b8d8f01568a7f24779fc
WENGELAWI MATHEWOS <MEETWENGEL@GMAIL.COM>
2023-11-28 16:56:45 -0600

    new practice folder

---
+++ W1D2/app.ts
@@ -0,0 +1,11 @@
+import wengel, {x,hello} from './helpers'
+
+console.log("hello from ts")
+
+const fullname:string="abebe is a good boy";
+
+console.log(`hello ${fullname}`)
+
+console.log(x)
+hello()
+wengel()
---
+++ W1D2/helpers.ts
@@ -0,0 +1,7 @@
+export const x=5;
+export function hello(){
+    console.log("hello from helpers")
+}
+export default function(){
+    console.log("hello from default")
+}
